8c187b8568f1ac33efd495e0ff8575236e87085c
YunusEmreSamat <you@example.com>
2021-04-01 20:06:53 +0300

    Ders 5 Calisma 1

--- Ders5MavenJunitProject/src/main/java/tr/edu/medipol/yazilimortamarac/Hesaplama.java
+++ Ders5MavenJunitProject/src/main/java/tr/edu/medipol/yazilimortamarac/Hesaplama.java
@@ -2,17 +2,21 @@ package tr.edu.medipol.yazilimortamarac;
 
 public class Hesaplama {
 
-	public static int toplama(int sayi1 , int sayi2) {
+	public static int toplama(int sayi1, int sayi2) {
 		return sayi1 + sayi2;
 	}
 	
-	public static int toplama(int [] sayilar) {
-		int toplam=0;
-		for(int i = 0; i<sayilar.length ; i++) {
-			toplam += sayilar[i];
-		}
-		
-		return toplam;
+	public static int cikarma(int sayi1, int sayi2) {
+		return sayi1 - sayi2;
 	}
+	
+	public static int carpma(int sayi1, int sayi2) {
+		return sayi1 * sayi2;
+	}
+	
+	public static int bolme(int sayi1, int sayi2) {
+		return sayi1 / sayi2;
+	}
+	
 	
 }
--- Ders5MavenJunitProject/src/test/java/tr/edu/medipol/yazilimortamarac/HesaplamaTest.java
+++ Ders5MavenJunitProject/src/test/java/tr/edu/medipol/yazilimortamarac/HesaplamaTest.java
@@ -7,14 +7,44 @@ public class HesaplamaTest {
 
 	@Test
 	public void testToplama() {
-		int asilSonuc1 = Hesaplama.toplama(2,2);
-		Assert.assertEquals(4, asilSonuc1);
+		int asilSonuc = Hesaplama.toplama(2, 2);
+		Assert.assertEquals(4, asilSonuc);
 		
-		Assert.assertEquals(0, Hesaplama.toplama(-2,2));
+		Assert.assertEquals(0, Hesaplama.toplama(-2, 2) );
 		
-		Assert.assertEquals(0, Hesaplama.toplama(-2,-2));
-
-		Assert.assertEquals(99999, Hesaplama.toplama(0,99999));
-
+		Assert.assertEquals(0, Hesaplama.toplama(2, -2) );
+		
+		Assert.assertEquals(99999, Hesaplama.toplama(0, 99999) );
 	}
+	
+	@Test
+	public void testCikarma() {
+		int asilSonuc = Hesaplama.cikarma(2, 2);
+		Assert.assertEquals(0, asilSonuc);
+		
+		Assert.assertEquals(2, Hesaplama.cikarma(4, 2) );
+		
+		Assert.assertEquals(6, Hesaplama.cikarma(10, 4) );
+	}
+	
+	@Test
+	public void testCarpma() {
+		int asilSonuc = Hesaplama.carpma(2, 2);
+		Assert.assertEquals(4, asilSonuc);
+		
+		Assert.assertEquals(8, Hesaplama.carpma(4, 2) );
+		
+		Assert.assertEquals(40, Hesaplama.carpma(10, 4) );
+	}
+	
+	@Test
+	public void testBolme() {
+		int asilSonuc = Hesaplama.bolme(2, 2);
+		Assert.assertEquals(1, asilSonuc);
+		
+		Assert.assertEquals(2, Hesaplama.bolme(4, 2) );
+		
+		Assert.assertEquals(3, Hesaplama.bolme(12, 4) );
+	}
+	
 }
